2b63112beb0a499023cd1c9e7ab648304df69ce6
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-06-02 19:56:55 +0900

    STL 공부하기

--- DataStruct/4.STL/4.STL/4.STL.cpp
+++ DataStruct/4.STL/4.STL/4.STL.cpp
@@ -23,6 +23,7 @@ using namespace std;
 //3.배열의 크기를 런타임중에 변경가능하다.
 void VectorMain()
 {
+	cout << "####### VectorMain ######" << endl;
 	vector<int> container(1);//컨테이너생성시 크기를 지정가능하다.
 	container[0] = 10;
 	cout << "Print:";
@@ -56,6 +57,24 @@ void VectorMain()
 	for (it = container.begin(); it != container.end(); it++)
 		cout << "[" << &*it << "]" << *it << ",";
 	cout << endl;
+
+
+	itFind = find(container.begin(), container.end(), 30);
+
+	cout << "Find(30):" << *itFind << endl;
+	itFind--;
+	cout << "Find(30)--:" << *itFind << endl;
+
+	itFind = find(container.begin(), container.end(), 10);
+
+	itFind+=4;
+	cout << "Find(10)+4:" << *itFind << endl;
+
+	cout << "PrintPtr[Insert]:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+
 	auto itErase = container.begin();
 	container.erase(itErase);
 	cout << "PrintPtr[Erase]:";
@@ -81,6 +100,7 @@ void VectorMain()
 //3.연결리스트의 종류: 단일, 환형, 이중 stl의 리스트는 어디에 해당되는가?
 void ListMain()
 {
+	cout << "####### ListMain ######" << endl;
 	//템플릿: 자료형을 변경할수있게함.
 	list<int> container(1);//컨테이너생성시 크기를 지정가능하다.
 	*container.begin() = 10;
@@ -107,13 +127,32 @@ void ListMain()
 	for (it = container.begin(); it != container.end(); it++)
 		cout << "[" << (int)&*it << "]" << *it << ",";
 	cout << endl;
-	list<int>::iterator itFind = container.end();
+	list<int>::iterator itFind = container.begin();
+	itFind++;
+	cout << "Find++:" << *itFind << endl;
+
 	container.insert(itFind, 30);
 
 	cout << "PrintPtr[Insert]:";
 	for (it = container.begin(); it != container.end(); it++)
 		cout << "[" << (int)&*it << "]" << *it << ",";
 	cout << endl;
+
+
+	itFind = find(container.begin(), container.end(), 30);
+
+	cout << "Find(30):" << *itFind << endl;
+	itFind--;
+	cout << "Find(30)--:" << *itFind << endl;
+
+	itFind = find(container.begin(), container.end(), 10);
+
+	itFind++;
+	itFind++;
+	itFind++;
+	itFind++;
+	cout << "Find(10)+4:" << *itFind << endl;
+
 	auto itErase = container.begin();
 	container.erase(itErase);
 	cout << "PrintPtr[Erase]:";
@@ -136,27 +175,146 @@ void ListMain()
 //데크: 앞뒤로 자료를 추가/삭제가능, 랜덤접근가능.
 void DequeMain()
 {
+	cout << "####### DequeMain ######" << endl;
+	deque<int> container(1);//컨테이너생성시 크기를 지정가능하다.
+	container[0] = 10;
+	cout << "Print:";
+	for (int i = 0; i < container.size(); i++)
+		cout << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+	container.resize(3); //배열의 크기를 지정한다.
+	cout << "Print:";
+	for (int i = 0; i < container.size(); i++)
+		cout << "[" << i << "]" << container[i] << ",";
+	cout << endl;
+	//1.추가 2.삽입 3.삭제 4.모두삭제
 
+	deque<int>::iterator it;
+
+	cout << "PrintPtr:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+
+	container.push_front(100);
+	container.push_back(20);
+
+	cout << "PrintPtr:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+	deque<int>::iterator itFind = container.end();
+	container.insert(itFind, 30);
+
+	cout << "PrintPtr[Insert]:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+
+
+	itFind = find(container.begin(), container.end(), 30);
+
+	cout << "Find(30):" << *itFind << endl;
+	itFind--;
+	cout << "Find(30)--:" << *itFind << endl;
+
+	itFind = find(container.begin(), container.end(), 10);
+
+	itFind += 4;
+	cout << "Find(10)+4:" << *itFind << endl;
+
+	cout << "PrintPtr[Insert]:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+
+	auto itErase = container.begin();
+	container.erase(itErase);
+	cout << "PrintPtr[Erase]:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+
+	container.push_back(40);
+	cout << "PrintPtr[insert]:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
+
+	container.clear(); //모두삭제
+	cout << "Clear:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
+	cout << endl;
 }
 //스택: 뒤에서 추가되고 뒤에서 꺼냄.
 //재귀함수에서 이전 함수를 호출할때마다 스택에 쌓임.
 //문자열뒤집기 -> 문자배열 -> apple -> elppa
 void StackMain()
 {
+	char strApple[] = "apple"; //배열의 크기를 지정하지않아도 문자열을 지정하면 할당할 메모리의 크기를 컴파일러가 체크하고 만들어줌.
+	stack<char> stackString;
 
+	int nStrSize = strlen(strApple);
+
+	for (int i = 0; i < nStrSize; i++)
+	{
+		cout << "[" << i << "]" << strApple[i] << endl;
+		stackString.push(strApple[i]);
+	}
+
+	int idx = 0;
+	while (stackString.empty() == false)
+	{
+		char cBack = stackString.top();
+		strApple[idx] = cBack;
+		cout << "[" << idx << "]" << strApple[idx] << endl;
+		stackString.pop();
+		idx++;
+	}
+
+	cout << "String:" << strApple << endl;
 }
 //큐: 뒤에서 추가하고 앞에서 꺼냄.
 //메세지큐: 이벤트가 발생한 순서대로 저장하는 공간.
 //입력된 순서대로 명령어 처리하기
 void QueueMain()
 {
+	queue<char> queMsg;
+	char cInput = 0;
+
+	while (cInput != 'x')
+	{
+		cin >> cInput;
+		queMsg.push(cInput);
+	}
+
+	while (queMsg.empty())
+	{
+		char cPop = queMsg.front();
+		cout << "Msg:" << cPop << endl;
+	}
 
 }
 //우선순위큐: 우선순위가 높은 원소가 먼저나감(힙)
 //무작위로 데이터를 넣었을때 어떤 순서대로 데이터가 나오는가? 큰값부터 나온다.
 void PriorytyQueueMain()
 {
+	priority_queue <char> queMsg;
+	char cInput = 0;
 
+	while (cInput != 'x')
+	{
+		cin >> cInput;
+		queMsg.push(cInput);
+	}
+
+	while (!queMsg.empty())
+	{
+		char cPop = queMsg.top();
+		cout << "Msg:" << cPop << endl;
+		queMsg.pop();;
+	}
 }
 //맵: 사전식으로 데이터를 찾을수있다.
 //해당영어단어를 넣으면 한국어 결과가 나온다.
@@ -205,11 +363,11 @@ void HashMapMain()
 void main()
 {
 	//VectorMain();
-	ListMain();
+	//ListMain();
 	//DequeMain();
 	//StackMain();
 	//QueueMain();
-	//PriorytyQueueMain();
+	PriorytyQueueMain();
 	//MapMain();
 	//SetMain();
 }
