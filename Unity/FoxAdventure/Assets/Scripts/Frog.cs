885dd6da6d2c26c631b640681d065efe6c86b9b3
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-06-24 19:32:53 +0900

    타이머 코루틴으로 변경

--- Unity/FoxAdventure/Assets/Scripts/Frog.cs
+++ Unity/FoxAdventure/Assets/Scripts/Frog.cs
@@ -12,17 +12,23 @@ public class Frog : MonoBehaviour
     public float JumpPower = 300;
     public bool isJumpping = false;
 
+    public float maxTime = 1; //최대시간
+    IEnumerator ProccssTimmer()
+    {
+        yield return new WaitForSeconds(maxTime);
+        isJump = true;
+    }
+
+    void SetTimmer()
+    {
+        StartCoroutine(ProccssTimmer());
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetTimmer();
     }
-
-    public float maxTime = 1; //최대시간
-    public float curTime = -1; //현재시간 //센티넬: 음수의 값을 설정해서 비활성화 상태를 만드는것.
-
-    public bool isGound;
 
     public void Move()
     {
@@ -43,23 +49,11 @@ public class Frog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (curTime >= 0) //현재타임 0보다 클때 //타이머가 활성화 됬을때
+        //UpdateTimmer();
+
+        if(isJumpping == false && isJump == false)
         {
-            if (curTime < maxTime) // 현재타임이 최대시간보다 작을때
-            {
-                curTime += Time.deltaTime; //현재시간에 델타타임값을 더한다.
-            }
-            else // 현재타임이 최대시간보다 작을때가 아닐때 // 현재시간이 최대시간값 클때,
-            {
-                isJump = true;
-                //현재시간에 -1을 셋팅한다 //타이머를 비활성화한다.
-                curTime = -1;
-            }
-        }
-        else //현재타임 0보다 클때 아닐때 //현재타임이 0보다 작을때 //마이너스 //타이머가 비활성화 상태일때
-        {
-            if(isJumpping == false)
-                curTime = 0; //타이머에 0의값을 설정한다 //타이머를 활성화 한다.
+            SetTimmer();
         }
 
         Jump();
@@ -76,7 +70,6 @@ public class Frog : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        isGound = false;
         Debug.Log($"OnCollisionEnter2D:{collision.gameObject.name}");
     }
 }
